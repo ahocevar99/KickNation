ae8e153a5b5570b9be436c9f07d057833838b6da
AndrejPC <andrej.hocevar11@gmail.com>
2025-03-03 23:38:18 +0100

    dysplay bonuses

--- frontend/src/components/Buy.jsx
+++ frontend/src/components/Buy.jsx
@@ -10,13 +10,17 @@ const Buy = ({alreadyReplaced}) => {
     const [money, setMoney] = useState();
     const [newPlayers, setNewPlayers] = useState([]);
     const [displayedPlayer, switchDisplayedPlayer] = useState(0);
+    const [isVisible, setVisibility] = useState(false)
     const location = useLocation();
     const { username } = location.state || {};
     
     
     const buyPack = async () => {
+        setNewPlayers([])
+        setVisibility(false)
         const response = await axios.get(`http://localhost:3000/buyPack?username=${username}`);
         setNewPlayers(response.data.squad)
+        setVisibility(true)
     }
 
     useEffect(() => {
@@ -47,7 +51,7 @@ const Buy = ({alreadyReplaced}) => {
     }
 
     return (
-        <div className='buy-container'>
+        <div className='buy-container' style={{height : isVisible? "630px" : "230px"}}>
             <div className='money-counter'>
                 <p>Money: {money} $</p>
             </div>
@@ -58,7 +62,7 @@ const Buy = ({alreadyReplaced}) => {
             <div>
                 {renderPlayer(displayedPlayer)}
             </div>
-            <div className="new-player-arrows">
+            <div className="new-player-arrows" style={{visibility : isVisible? "visible": "hidden"}} >
                 <i className="fa-solid fa-arrow-left" onClick={() => changePlayer()}></i>
                 <i className="fa-solid fa-arrow-right" onClick={() => changePlayer()}></i>
             </div>
